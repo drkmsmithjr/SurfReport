41dfbffaf7caf25b773b39359c77204c5c496aa3
Mark Smith <drkmsmithjr@gmail.com>
2018-01-06 16:39:27 -0800

    Improved Error Checking
    Improved the error checking with the script file.  This now will go
    through all the surf spots and check to see if any errors occurred..
    Im see that some skills and intents were returning errors.  Improved the
    error checking with the surf report.

--- mySurfReportSurfline.py
+++ mySurfReportSurfline.py
@@ -2,15 +2,11 @@
 mySurfReport
 
 #Written by Mark Smith, www.surfncircuits.com
-#Based on Code Written by Colin Karpfinger
-# http://punchthrough.com/bean
-# http://punchthrough.com/bean/examples/surf-report-notifier/
-# https://github.com/PunchThrough/BeanSurfMap
-# copyright (c) 2014 Punch Through Design
-"""
 
+"""
 #
 from surfreport import SurfSpot,spots, getsurfspots
+import time
 #from surfreport import spots
 
 if __name__ == "__main__":
@@ -24,36 +20,83 @@ if __name__ == "__main__":
     #print(report.printReport(0))
     #report.getWaterTemp()
     #print(report.printWaterTemp())
+    
+    numsurfreports = 0
+    numbestday = 0
+    numtidereports = 0
+    numwatertemps = 0
+    errorMsg = ""
+    getMsg = ""
+    failedreportmessages = []
+    
     for spot in spots:
         report = SurfSpot(spot, spots[spot][0], spots[spot][1], spots[spot][2], spots[spot][3])
-        reports.append(report)
-        #print(spot)
-        #break
-        
+        reports.append(report)  
      
     for r in reports:
         # we need to call tehse reports to get info and search
         #try:
         print(r.spotName)
-        r.getReport()
-        r.getTideReport()
-        for x in range(0,6):
-           print("  " + r.printReport(x))
-        for x in range(0,6):
-           print("  " + r.printTideReport(x))
-        r.getWaterTemp()
-        print("  " + r.printWaterTemp())
-           
-        #except:
-        #   print("there was an issue getting the tide report: please try later") 
-        
-           #print(r.printBestDayToSurf())
-        #print("On this day ")
-        #print(r.printReport(r.bestdaytosurf))  
-        #try:
-        #   r.getTideReport()
-        #   print(r.printTideReport(2))
-        #except:
-        #   print("there was an issue getting the tide report: please try later")
-                
-           #break
+        try:
+           r.getReport()
+           numsurfreports += 1
+           for x in range(0,6):
+              print("  " + r.printReport(x))
+        except:
+           errorMsg = "ERROR getting surf report/best day for " + r.spotName 
+           print(errorMsg) 
+           failedreportmessages.append(errorMsg)
+        try:      
+           print("  " + r.printBestDayToSurf())
+           numbestday += 1
+        except:
+           errorMsg = "ERROR getting surf BEST DAY for " + r.spotName
+           print(errorMsg)
+           failedreportmessages.append(errorMsg)
+        #time.sleep(0.5)   
+   
+    for r in reports:
+       # we need to call tehse reports to get info and search
+       #try:
+       print(r.spotName) 
+       try:         
+          r.getTideReport()
+          for x in range(0,6):
+             print("  " + r.printTideReport(x))
+          numtidereports += 1
+       except:
+          errorMsg = "ERROR getting the tide report for " + r.spotName
+          print(errorMsg)
+          failedreportmessages.append(errorMsg)
+          TEST_FAILED = True
+       #time.sleep(0.5)
+    for r in reports:
+       # we need to call tehse reports to get info and search
+       #try:
+       print(r.spotName)
+       #r.getWaterTemp()
+       #print("  " + r.printWaterTemp())         
+       try:
+          r.getWaterTemp()
+          getMsg = "  " + r.printWaterTemp()
+          if "Sorry" in getMsg:
+             getMsg = "ERROR getting the water temp for " + r.spotName
+             failedreportmessages.append(getMsg)
+          else:
+             numwatertemps += 1   
+          print(getMsg)         
+       except:
+          errorMsg = "ERROR getting the water temp for " + r.spotName
+          print(errorMsg)
+          failedreportmessages.append(errorMsg)
+       #time.sleep(0.5) 
+    print("The total reports available: " + str(len(reports)))          
+    print("Successful Surf reports : " + str(numsurfreports))
+    print("Successful best Day reports: " + str(numbestday))
+    print("Successful tide reports: " + str(numtidereports))
+    print("Successful water temp reports: " + str(numwatertemps))
+    for x in failedreportmessages:
+       print (x)
+       
+    
+    
